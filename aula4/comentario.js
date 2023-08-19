719ce22287058913b9a34b2c355e486904e637e8
Ronaldo Faustino <ohnirucse@msn.com>
2023-08-18 22:15:34 -0300

    Tipos de Comentario

---
+++ aula4/comentario.js
@@ -0,0 +1,12 @@
+// É escrever um comentário
+console.log('Olá Mundo')
+console.log('Linha 1')
+console.log('Linha 2')
+console.log('Linha 3')
+console.log('Linha 4')
+console.log('Linha 5')
+
+/**
+ * Um bloco
+ * de código
+ */

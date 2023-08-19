20fea5d59b1fae8f411d0f7201251ce21eb346cb
Ronaldo Faustino <ohnirucse@msn.com>
2023-08-18 22:03:08 -0300

    Exemplo

--- aula3/index.js
+++ aula3/index.js
@@ -3,3 +3,5 @@ console.log("Teste log");
 console.log(`Teste log`);
 
 console.log(123456, 15.85, 'Test log')
+
+console.log('Meu test "console.log()". Estou aprendendo JavaScript às',10,'da manhã')
